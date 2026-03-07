d06ab9ba927da43649f11e25619b120f2c94f3ba
tim-da <d.timoshin@ami-business.com>
2026-03-07 06:46:46 +0300

    Address latest Greptile feedback on deps and Follow model

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -84,3 +84,6 @@ class Follow(models.Model):
                 name='prevent_self_follow',
             ),
         ]
+
+    def __str__(self):
+        return f'{self.user.username} -> {self.author.username}'
